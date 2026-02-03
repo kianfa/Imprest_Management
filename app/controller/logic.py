43410317ca6b643626f10ac12b09d8de346d6961
Nozhan Ghayati <ghayati2006@gmail.com>
2026-02-03 18:07:16 +0330

    loading DB:)

--- app/controller/logic.py
+++ app/controller/logic.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional
 from PyQt6.QtWidgets import QFileDialog, QWidget
 from dataclasses import dataclass
+from app.data import user_repository as repo
+from PyQt6.QtGui import QStandardItem, QStandardItemModel
 
 
 class receipt_entry_logic:
@@ -28,6 +30,7 @@ class receipt_entry_logic:
         return file_path or None
 
 
+
 @dataclass
 class LoginResult:
     ok: bool
@@ -45,3 +48,48 @@ class main_window_logic:
         # later: check DB / API
         return self.validate_inputs(username, password)
 
+
+
+class calling_page_logic:
+    def __init__(self):
+        super().__init__()
+
+
+    def load_invoices(self):
+        rows = []
+        if self.radioButton1.isChecked():
+            rows = repo.load_data.get_invoices_by_invoice_no(
+                self.leInvoiceNo.text()
+            )
+
+        elif self.radioButton2.isChecked():
+            rows = repo.load_data.get_invoices_by_registration_date(
+                self.deRegstrationDate.text()
+            )
+
+        elif self.radioButton3.isChecked():
+            rows = repo.load_data.get_invoices_by_login_date(
+                self.deLoginStart.text()
+            )
+
+        elif self.radioButton4.isChecked():
+            rows = repo.load_data.get_invoices_by_explanation(
+                self.leExplanation.text()
+            )
+
+        self.populate_table(rows)
+
+
+    def populate_table(self, rows):
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels([
+            "Invoice No", "Title","amount" , "Explanation",
+            "Registration Date", "Login Date"
+        ])
+
+        for row in rows:
+            model.appendRow(
+                [QStandardItem(str(col)) for col in row]
+            )
+
+        self.tableView.setModel(model)
--- app/ui/Calling_Page.py
+++ app/ui/Calling_Page.py
@@ -2,12 +2,12 @@ from PyQt6.QtWidgets import QWidget
 from PyQt6.QtCore import QDate
 from PyQt6.uic import loadUi
 from pathlib import Path
-
+from app.data.user_repository import load_data
+from app.controller.logic import calling_page_logic
 
 class Calling_Page(QWidget):
     def __init__(self):
         super().__init__()
-
         ui_path = Path(__file__).parent / "Calling_Page.ui"
         loadUi(ui_path, self)
         self.stackedWidget.setCurrentIndex(3)
@@ -27,10 +27,27 @@ class Calling_Page(QWidget):
         self.dateEdit_2.setDate(self.dateEdit_2.minimumDate())
 
         # DateEdit 3 (End)
-        self.dateEdit_3.setMinimumDate(QDate(2000, 1, 1))
-        self.dateEdit_3.setSpecialValueText("End")
-        self.dateEdit_3.setDisplayFormat("yyyy-MM-dd")
-        self.dateEdit_3.setDate(self.dateEdit_3.minimumDate())
+        self.deLoginStart.setMinimumDate(QDate(2000, 1, 1))
+        self.deLoginStart.setSpecialValueText("End")
+        self.deLoginStart.setDisplayFormat("yyyy-MM-dd")
+        self.deLoginStart.setDate(self.deLoginStart.minimumDate())
+
+        #loading data
+        repo = load_data()
+        self.logic = calling_page_logic()
+        self.logic.tableView = self.tableView
+
+        self.logic.radioButton1 = self.radioButton1
+        self.logic.radioButton2 = self.radioButton2
+        self.logic.radioButton3 = self.radioButton3
+        self.logic.radioButton4 = self.radioButton4
+
+        self.logic.leInvoiceNo = self.leInvoiceNo
+        self.logic.leExplanation = self.leExplanation
+        self.logic.deRegstrationDate = self.deRegstrationDate
+        self.logic.deLoginStart = self.deLoginStart
+
+        self.btnSearch.clicked.connect(self.logic.load_invoices)
 
     def change_page(self, index):
         if self.sender().isChecked():
