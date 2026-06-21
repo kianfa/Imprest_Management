612ca9b36469ea8eb8baa06ddbb2a298d0daa625
Nozhan Ghayati <ghayati2006@gmail.com>
2026-06-21 20:47:53 +0330

    defining function that check the license time and show error to user

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from app.ui.main_window_UI import MainWindow
 from app.data.data_base import DataBase
+from app.controller.license import LicenseManager
 
 #pyqt6-tools designer
 #.\.venv2\Scripts\activate
@@ -11,6 +12,8 @@ def main() -> None:
     DataBase.create_tables()
     DataBase.create_default_users()
     app = QApplication(sys.argv)
+    lm = LicenseManager()
+    lm.check_or_initialize()
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
